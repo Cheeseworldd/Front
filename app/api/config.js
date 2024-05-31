45377eeaf54be3c4f403ccd7ed06888f5dc2d81c
Cheeseworldd <87156906+Cheeseworldd@users.noreply.github.com>
2024-05-31 22:25:40 +1000

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://baack.nomorepartiesco.ru/api";
+export const BASE_URL = "https://frront.nomorepartiesco.ru/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
